d1176b5126f68c83cc782784b4be426fea989935
Luis Castillo <mr.luiscastillo@gmail.com>
2022-03-30 23:35:10 -0400

    add token to localStorage for signUp

--- landing/js/script-landing.js
+++ landing/js/script-landing.js
@@ -75,7 +75,7 @@ function submitSignUpForm(event) {
     .then(response => response.json())
     .then(data => {
         console.log(data);
-        
+        localStorage.setItem('user', data.jwt);
         if (data.error) {
           alert("Error Password or Username"); /*displays error message*/
         } 
